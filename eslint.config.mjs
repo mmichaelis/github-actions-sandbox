1d4f4379034f64b432380e1a4990d97ff720f61c
Mark Michaelis <thragor@mailbox.org>
2024-05-22 22:34:50 +0200

    build(eslint): Add Linting for YAML Files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,16 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import pluginYml from "eslint-plugin-yml";
 
 export default [
-  { languageOptions: { globals: globals.node } },
+  {
+    ignores: [".idea/*", "pnpm-lock.yaml", "node_modules/*"],
+  },
+  {
+    languageOptions: { globals: globals.node },
+  },
   pluginJs.configs.recommended,
   pluginPrettierRecommended,
+  ...pluginYml.configs["flat/recommended"],
 ];
